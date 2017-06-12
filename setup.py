09d0cc734257c5746308cf6bd09aaf3707c43de2
Connor Brinton <connor.brinton@gmail.com>
2017-06-12 09:38:25 -0700

    Bump version number to 0.1.5.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ SCRIPTS = ['scripts/lcat-featurize.py',
 
 setup(name='lcat',
       packages=PACKAGES,
-      version='0.1.4',
+      version='0.1.5',
       description='Lung Cancer Action Team (LCAT) toolkit',
       author='LCAT Team',
       author_email='brinton@cs.stanford.edu',
